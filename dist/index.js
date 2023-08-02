ad9a6ad9f23d138b577c5db6f5775948046519df
Rhia-nnon <134813267+Rhia-nnon@users.noreply.github.com>
2023-08-01 23:42:39 -0400

    mejoras

---
+++ dist/index.js
@@ -0,0 +1,27 @@
+"use strict";
+let extincionDinosaurios = 76000000;
+let DinosaurioFavorito = "T-Rex";
+let Extintos = true;
+function chanchitofeliz(config) {
+    return config;
+}
+let animales = ["chanchito", "perrito", "gatito"];
+let nums = [1, 2, 3];
+let checks = [];
+let nums2 = [];
+let tupla = [1, ["chanchito feliz", "chanchito hola"]];
+tupla.push(12);
+const chica = "s";
+const mediana = "m";
+const grande = "l";
+const extragrande = "xl";
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+
+let extincionDinosaurios: number = 76_000_000
+let DinosaurioFavorito: string = "T-Rex"
+let Extintos: boolean = true
+
+function chanchitofeliz(config: any) {
+ return config
+}
+
+let animales: string[] = ["chanchito","perrito","gatito"]
+let nums: number[] = [1, 2, 3]
+let checks: boolean[] = []
+let nums2: Array<number> = []
+
+// nums.map(x => x.) el autocompletado sugiere métodos del tipo de dato
+
+let tupla: [number, string[]] = [1, ["chanchito feliz", "chanchito hola"]]
+tupla.push(12)
+
+const chica = "s"
+const mediana = "m"
+const grande = "l"
+const extragrande ="xl"
+
+enum Talla { Chica = "s", Mediana = "m", Grande = "l", ExtraGrande = "xl" }
+
+const variable1= Talla.Grande
+
+console.log(variable1)
+
+const objeto = { id: 1, nombre: "Hola Mundo" }
+objeto.nombre = "Hola Mundo"
